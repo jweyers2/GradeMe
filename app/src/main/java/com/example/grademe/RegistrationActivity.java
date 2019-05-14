c7042395c82bb43e2d1bcbe2d9074a24b1483ed2
tschnee3 <tschnee3@smail.uni-koeln.de>
2019-05-14 15:26:50 +0200

    Login and Registration Form Update

--- app/src/main/java/com/example/grademe/RegistrationActivity.java
+++ app/src/main/java/com/example/grademe/RegistrationActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 
 /**
  * Created by tutlane on 08-01-2018.
@@ -13,13 +14,14 @@ import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
-        TextView login = (TextView)findViewById(R.id.lnkLogin);
-        System.out.print(login.getText());
+        login = (TextView)findViewById(R.id.lnkLogin);
+       // System.out.print(login.getText());
         login.setMovementMethod(LinkMovementMethod.getInstance());
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +30,13 @@ public class RegistrationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button button = findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
     }
 }
