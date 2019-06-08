d7f82591b7e54a562f05439aa7aeb87a02974ede
Ducktard <johannes_weyers@gmx.de>
2019-06-08 19:37:35 +0200

    changed data structure -added grade view (categoryRating)

---
+++ app/src/main/java/com/example/grademe/CategoryRatingListAdapter.java
@@ -0,0 +1,54 @@
+package com.example.grademe;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.grademe.domain.CategoryRating;
+import com.example.grademe.domain.Pupil;
+
+import java.util.List;
+
+public class CategoryRatingListAdapter extends BaseAdapter {
+
+    private List<CategoryRating> grades;
+    Context context;
+
+    private static LayoutInflater inflater = null;
+    public CategoryRatingListAdapter(Context context, List<CategoryRating> grades){
+        this.context = context;
+        this.grades = grades;
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return grades.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return grades.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return grades.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi = convertView;
+        if (vi == null)
+            vi = inflater.inflate(R.layout.category_rating_list_row, null);
+        TextView header = (TextView) vi.findViewById(R.id.header);
+        header.setText("Lehrerbewertung: " + grades.get(position).getRatingTeacher() + " Schülerselbstbewertung: " + grades.get(position).getRatingPupil());
+//        TextView rowContentLeft = (TextView) vi.findViewById(R.id.rowContentLeft);
+//        rowContentLeft.setText("Noteneinträge : " + grades.get(position).getCategoryRatingList().size());
+        return vi;
+    }
+}
---
+++ app/src/main/java/com/example/grademe/NotenFragment.java
@@ -0,0 +1,93 @@
+package com.example.grademe;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.grademe.Model.CategoryRatingModel;
+import com.example.grademe.Model.ModulesModel;
+import com.example.grademe.Model.PupilsModel;
+import com.example.grademe.domain.CategoryRating;
+import com.example.grademe.domain.Pupil;
+
+import java.util.List;
+
+/**
+ * Created by user on 12/31/15.
+ */
+
+public class NotenFragment extends Fragment {
+
+    View myView;
+    private ListView listView;
+    private CategoryRatingModel grades;
+
+    private SessionManager session;
+    private Button joinButton;
+    FragmentManager fragmentManager;
+
+
+    public static NotenFragment newInstance(List<CategoryRating> grades) {
+
+        NotenFragment fragment = new NotenFragment();
+        fragment.grades = new CategoryRatingModel(grades);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        session = ((GradeMeApp)getActivity().getApplication()).getSessionManager();
+
+        fragmentManager = getFragmentManager();
+        myView = inflater.inflate(R.layout.kurse_layout, container, false);
+        listView = (ListView) myView.findViewById(R.id.listViewModules);
+
+
+        ListAdapter listAdapter = new CategoryRatingListAdapter(getActivity(),grades.getGrades().getValue());
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                fragmentManager.beginTransaction()
+//                        .replace(R.id.content_frame
+//                                ,SchuelerFragment.newInstance(grade.getModules().getValue().get(position).getPupils()))
+//                        .commit();
+            }
+        });
+        listView.setAdapter(listAdapter);
+
+        joinButton = (Button) myView.findViewById(R.id.joinModule);
+        String role = session.getUserDetails().get(session.KEY_ROLE);
+        if(role.equals("teacher"))
+        {
+           // joinButton.setV;
+             joinButton.setText("Kurs erstellen");
+            //TODO Kurs Erstellung aufrufen
+        }
+        else
+        {
+            joinButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), QRActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+        return myView;
+    }
+
+
+
+}
